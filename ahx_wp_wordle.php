8eb23cfbfc33e2a3fe40cae17aef169c5dbf44f7
Wordpress DEV (AHX) <alexander@familie-herbst.net>
2026-05-24 23:49:38 +0200

    Update keyboard styles for improved layout; adjust gaps and text colors for better visibility (refs #9; fixes #9)

--- ahx_wp_wordle.php
+++ ahx_wp_wordle.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: AHX WP Wordle
 Description: Stellt ein Wordle-Spiel als Shortcode bereit.
-Version: v1.4.2
+Version: v1.4.3
 Author: AHX
 Text Domain: ahx_wp_wordle
 Domain Path: /languages
